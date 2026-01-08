bdfef02173b3917be88d340ed44a44aeaa44740c
utsavg05 <utsav.gp1204@gmail.com>
2026-01-08 22:27:16 +0530

    removed unnecessary files

--- app/logout/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-import { createClient } from "@/lib/supabase/server";
-
-export async function POST() {
-  const supabase = await createClient();
-  await supabase.auth.signOut();
-
-  return NextResponse.redirect("http://localhost:3000");
-}
